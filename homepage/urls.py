0da3bb69a0405b342837693aa3f71d6de653e282
prometfire <prometfire@gmail.com>
2013-06-15 23:50:54 +0300

    made url dispatcher to homepage app

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+# from .views import homepage_view
 
-urlpatterns = patterns('',
+urlpatterns = patterns('homepage.views',
     # Examples:
-    url(r'^$', include('homepage.urls')),
+    url(r'^$', 'homepage_view'),
     # url(r'^prometfire/', include('prometfire.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
--- homepage/views.py
+++ homepage/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def homepage_view(request):
+	return render_to_response('homepage.html',
+							 {'name': 'bob'}
+	)
