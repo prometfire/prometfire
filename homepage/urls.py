3da1845213539041847140b758ecf4482ddbb060
prometfire <prometfire@gmail.com>
2013-06-19 23:55:26 +0300

    form test

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('homepage.views',
     url(r'^$', 'homepage_view'),
     url(r'^welcome/', 'welcome_view'),
+    url(r'^form/', 'form_view'),
+    url(r'^form_handler/', 'form_handler'),
 )
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage_view(request):
 	return render_to_response('homepage.html',
 							 {'name': 'bob'}
-	)
+							 )
 
 def welcome_view(request):
 	return HttpResponse('Welcome')
+
+def form_view(request):
+	return render(request, 'form.html')
+
+def form_handler(request):
+	return HttpResponse('got it! %s' % request.GET['name'])
